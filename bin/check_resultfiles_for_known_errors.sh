d0d1df98ece44c5921962b1fa1e6430a87c98981
Yauhen Yakimovich <yauhen.yakimovich@uzh.ch>
2012-10-24 18:39:07 +0200

    modified:   README - Corrected crontab-wrapper
    modified:   bin/check_resultfiles_for_known_errors.sh
    modified:   bin/checkoutoffocus.sh
    modified:   bin/createplateoverview.sh
    modified:   bin/datafusion.sh
    modified:   bin/submitbatchjobs.sh
    - Synced with HOME folder on Brutus (contains a number of bugfixes
      and inline code replacing *.command calls).
    
    new file:   tests/mock/project/PreCluster_simple.mat
    - A very simple CP pipeline for the mock dataset.

--- bin/check_resultfiles_for_known_errors.sh
+++ bin/check_resultfiles_for_known_errors.sh
@@ -31,32 +31,53 @@ fi
 # echo $(find $BATCHDIR -maxdepth 1 -name "${RESULTFILESUFFIX}*.results")
 ### 
 
+ERRORCAUSE="[UNKNOWN]"
+ERRORGREP=""
 for resultFile in $(ls $BATCHDIR/${RESULTFILESUFFIX}*.results -tr 2>/dev/null | tail -1); do
   #echo "$resultFile: "
   if ([ $(cat $resultFile | grep "TERM_RUNLIMIT" | wc -l ) -gt 0 ] ||
       [ $(cat $resultFile | grep "TERM_OWNER" | wc -l ) -gt 0 ] ||
-      [ $(cat $resultFile | grep "TERM_ADMIN" | wc -l ) -gt 0 ] ||
       [ $(cat $resultFile | grep "Problem calling GhostScript" | wc -l ) -gt 0 ] ||
       [ $(cat $resultFile | grep "Opening log file:" | wc -l ) -gt 0 ] ||
       [ $(cat $resultFile | grep "Assertion detected at" | wc -l ) -gt 0 ] ||
       [ $(cat $resultFile | grep "matlab: command not found" | wc -l ) -gt 0 ] ||
+      [ $(cat $resultFile | grep "DISABLEDBYBEREND IMRESIZE" | wc -l ) -gt 0 ] ||
+      [ $(cat $resultFile | grep "Undefined function or method " | wc -l ) -gt 0 ] ||
       [ $(cat $resultFile | grep "License Manager Error" | wc -l ) -gt 0 ]);
   then
     if [ $(cat $resultFile | grep "TERM_RUNLIMIT" | wc -l ) -gt 0 ]; then
-      echo "        [KNOWN ERROR FOUND]: Job exceeded runlimit, resetting job & placing timeout flag file"
-      touch $(dirname $SUBMITTEDFILE)/$(basename $SUBMITTEDFILE .submitted).runlimit
+	if [ -e $(dirname $SUBMITTEDFILE)/$(basename $SUBMITTEDFILE .submitted).runlimit ]; then
+		echo "<warning>"
+    		echo " Error: Job  $(basename $resultFile) timed out too many times."
+    		echo "      <result_file>$resultFile</result_file>"
+    		echo "</warning>"
+		return
+                ERRORCAUSE="[KNOWN] Timed out too many times."
+	else
+		echo "        [KNOWN ERROR FOUND]: Job exceeded runlimit, resetting job and placing timeout flag file"
+      		touch $(dirname $SUBMITTEDFILE)/$(basename $SUBMITTEDFILE .submitted).runlimit
+                ERRORCAUSE="[KNOWN] Timed out; Trying in longer queue."
+	fi
     elif [ $(cat $resultFile | grep "TERM_OWNER" | wc -l ) -gt 0 ]; then
       echo "        [KNOWN ERROR FOUND]: Owner terminated job, resetting job"
-    elif [ $(cat $resultFile | grep "TERM_ADMIN" | wc -l ) -gt 0 ]; then
-      echo "        [KNOWN ERROR FOUND]: Administrator terminated job, resetting job"
+      ERRORCAUSE="[KNOWN] Owner terminated job"
     elif [ $(cat $resultFile | grep "License Manager Error" | wc -l ) -gt 0 ]; then
-      echo "        [KNOWN ERROR FOUND]: Matlab License Magager Error, resetting job"
+      echo "        [KNOWN ERROR FOUND]: Matlab License Manager Error, resetting job"
+      ERRORCAUSE="[KNOWN] Matlab License Manager Error"
+      ERRORGREP=$(cat $resultFile | grep -A2 "Matlab License Manager Error")
     elif [ $(cat $resultFile | grep "License Manager Error -15" | wc -l ) -gt 0 ]; then
-      echo "        [KNOWN ERROR FOUND]: Matlab License Magager Error (-15), resetting job"
+      echo "        [KNOWN ERROR FOUND]: Matlab License Manager Error (-15), resetting job"
+      ERRORCAUSE="[KNOWN] Matlab License Manager Error (-15)"
     elif [ $(cat $resultFile | grep "Problem calling GhostScript" | wc -l ) -gt 0 ]; then
       echo "        [KNOWN ERROR FOUND]: Problem calling GhostScript, resetting job"
+      ERRORCAUSE="[KNOWN] Problem calling GhostScript"
+    elif [ $(cat $resultFile | grep "Undefined function or method " | wc -l ) -gt 0 ]; then
+      echo "        [KNOWN ERROR FOUND]: MATLAB: Undefined function or method ... missing function, which gets temporarily restarted"
+      ERRORCAUSE="[KNOWN] MATLAB: Undefined function or method"
+      ERRORGREP=$(cat $resultFile | grep -A2 "Undefined function or method ")
     elif [ $(cat $resultFile | grep "matlab: command not found" | wc -l ) -gt 0 ]; then
       echo "        [KNOWN ERROR FOUND]: known environment error: matlab:command not found, resetting job"
+      ERRORCAUSE="[KNOWN] Environment error: Matlab command not found"
     fi
     echo "          removing $resultFile"
     rm $resultFile
@@ -64,15 +85,23 @@ for resultFile in $(ls $BATCHDIR/${RESULTFILESUFFIX}*.results -tr 2>/dev/null | 
       echo "          removing $SUBMITTEDFILE"
       rm $SUBMITTEDFILE
     fi
+    # We could create a little error-log that links to the file and reports the error-type, which could be shown on the main iBRAIN page.
   elif [ $(cat $resultFile | grep -i -e "Error" | wc -l ) -gt 0 ]; then
     echo "<warning>"
     echo " Unknown error found in result file $(basename $resultFile)"
     echo "      <result_file>$resultFile</result_file>"
     echo "</warning>"
+    ERRORCAUSE="[UNKNOWN] Unknown error found"
+    ERRORGREP=$(cat $resultFile | grep -A2 "Error")
   else
   ### THERE IS NO 'ERROR' (KNOWN OR UNKNOWN) IN THE FILE...
     echo "      <warning>Strangely, no known or unknown errors were found. <result_file>$resultFile</result_file></warning>"
+    ERRORCAUSE="[UNKNOWN] No error found"
   fi
+
+  # Append result file link and description to some log file
+  echo "<RESULTFILE>$resultFile</RESULTFILE><ERRORDESCRIPTION>$ERRORCAUSE</ERRORDESCRIPTION><ERRORTIME>$(date +'%Y%m%d%H%M')</ERRORTIME><ERRORGREP>$ERRORGREP</ERRORGREP>" >> ~/2NAS/Data/Code/iBRAIN/database/errorlog.xml
 done
 ###
+
 
--- bin/checkoutoffocus.sh
+++ bin/checkoutoffocus.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-for inputfilename in $(find $1 -name "Measurements_*BlueSpectrum.mat" -type f -maxdepth 1); do
+for inputfilename in $(find $1 -maxdepth 1 -type f -name "Measurements_*BlueSpectrum.mat"); do
 
 PATHNAME=$(dirname $inputfilename)
 
@@ -8,12 +8,20 @@ if [ ! -r $PATHNAME/Measurements_Image_OutOfFocus.mat ]; then
 
 echo submitting outoffocus scoring on $PATHNAME
 REPORTFILE=CheckOutOfFocus_$(date +"%y%m%d%H%M%S").results
-bsub -W 7:00 -o $PATHNAME/$REPORTFILE ./CheckOutOfFocus/CheckOutOfFocus.command $PATHNAME
-
+bsub -W 8:00 -o $PATHNAME/$REPORTFILE "matlab -singleCompThread -nodisplay -nojvm << M_PROG; 
+check_outoffocus('$PATHNAME');
+M_PROG"
+touch $(dirname $1)/CheckOutOfFocus.submitted
 else
 
 echo skipped $PATHNAME for outoffocus scoring
 
+if [ ! -e $(dirname $1)/CheckOutOfFocus.submitted ]; then
+touch $(dirname $1)/CheckOutOfFocus.submitted
+fi
+
 fi
 
 done
+
+touch $(dirname $1)/CheckOutOfFocus.submitted
