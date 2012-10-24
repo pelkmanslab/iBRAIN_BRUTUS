1642a7d6c449ebfdba990379a8cdc9fde36c7961
Yauhen Yakimovich <yauhen.yakimovich@uzh.ch>
2012-10-24 16:53:25 +0200

    modified:   bin/precluster.sh Synchronizing to the latest version from the cluster HOME folder. Apparently previous was outdated.

--- bin/precluster.sh
+++ bin/precluster.sh
@@ -11,7 +11,16 @@ echo precluster.sh $2 is not readable
 exit 1
 fi
 
-bsub -W 7:00 -o $1/BATCH/PreCluster_$(date +"%y%m%d%H%M%S").results ~/PreCluster/PreCluster.command $2 $1/TIFF/ $1/BATCH/
+# echo "PreCluster $2 $1/TIFF/ $1/BATCH/" > ~/temp.m
+### THIS SEEMS TO WORK!
+#bsub -W 8:01 -o $1/BATCH/PreCluster_$(date +"%y%m%d%H%M%S").results "matlab -singleCompThread -nodisplay -nojvm < ~/temp.m"
+
+bsub -W 8:00 -o $1/BATCH/PreCluster_$(date +"%y%m%d%H%M%S").results "matlab -singleCompThread -nodisplay -nojvm nojit << M_PROG; 
+check_missing_images_in_folder('${1}/TIFF/');
+PreCluster_with_pipeline('${2}','${1}/TIFF/','${1}/BATCH/'); 
+M_PROG"
+
 
 touch $1/PreCluster.submitted
+
 
