a56b245a3aac69939891b7c7503b9015e1a7fb59
Yauhen Yakimovich <eugeny.yakimovitch@gmail.com>
2013-08-17 23:07:28 +0200

    modified:   startup.m - Update to the latest startup.m mostly pointing to ~/code/*

--- startup.m
+++ startup.m
@@ -13,7 +13,7 @@ fprintf('%s: disabling all warnings\n',mfilename)
 ibrainroot = pwd;
 if ~fileattrib([ibrainroot filesep 'etc' filesep 'config' ])
     disp(['Missing ibrainroot folder and/or etc/config: ' ibrainroot ])
-else
+else 
     [result, ibrain_matlab_path] = system(['IBRAINROOT=' ibrainroot ';. $IBRAINROOT/etc/config; if [ "$IBRAIN_MATLAB_PATH" == "" ];then exit 1;fi ; echo $IBRAIN_MATLAB_PATH']);
     ibrain_matlab_path = deblank(ibrain_matlab_path);
     if result ~= 0
@@ -23,22 +23,24 @@ else
         disp(['Missing ' ibrainroot filesep 'pathdef.m file; recreating it...'])
 
         % Add iBRAIN MATLAB code to path
-        %ibrain_matlab_path = '/BIOL/imsb/fs2/bio3/bio3/Data/Code/iBRAIN/matlab';
-        disp(['Adding path (with subfolders): ' ibrain_matlab_path])
+        %ibrain_matlab_path = '~/code/dep';
+        disp(['Resetting path and including subfolders of (as well as sibling repositories if found): ' ibrain_matlab_path])
         addpath(ibrain_matlab_path);
-        %the_path = create_path(ibrain_path, {}); % no ignore dirs
-        the_path = create_path(ibrain_matlab_path, {['Berend' filesep 'image']});
-        addpath(the_path);
-
-        % Prepend compiled tlbxs
-        cmt.prepend_compiled_path();
+        resetpath();
+        disp('done.')
+         
 
         % Add some other matlab folders (note ~/matlab added anyway).
-
+        
         % Save file.
-        savepath([ibrainroot filesep 'pathdef.m']);
+        pathdef_file = fullfile(ibrainroot, 'pathdef.m');
+        disp(['Saving path into: ' pathdef_file])
+        savepath(pathdef_file);
     end
 end
 
-clear();
+clear all;
+
+% Prepend compiled tlbxs
+cmt.load();
 
