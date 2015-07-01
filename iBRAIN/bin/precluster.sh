37b85a2e8bd1d404594c9e4474513dc6314d092c
Yauhen Yakimovich <eugney.yakimovitch@gmail.com>
2015-07-01 16:00:51 +0200

    Submit precluser step with 'jvm'. After upgrade to matlab 2013a this CP is now required to be run with java.

--- iBRAIN/bin/precluster.sh
+++ iBRAIN/bin/precluster.sh
@@ -15,7 +15,7 @@ fi
 ### THIS SEEMS TO WORK!
 #bsub -W 8:01 -o $1/BATCH/PreCluster_$(date +"%y%m%d%H%M%S").results "matlab -singleCompThread -nodisplay -nojvm < ~/temp.m"
 
-bsub -W 8:00 -o $1/BATCH/PreCluster_$(date +"%y%m%d%H%M%S").results "matlab -singleCompThread -nodisplay -nojvm nojit << M_PROG; 
+bsub -W 8:00 -o $1/BATCH/PreCluster_$(date +"%y%m%d%H%M%S").results "matlab -singleCompThread -nodisplay << M_PROG; 
 check_missing_images_in_folder('${1}/TIFF/');
 PreCluster_with_pipeline('${2}','${1}/TIFF/','${1}/BATCH/'); 
 M_PROG"
