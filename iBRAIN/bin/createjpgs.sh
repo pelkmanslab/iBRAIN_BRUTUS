3a9ef415972fb7794789ae9fea9dc19f45e9cde7
Yauhen Yakimovich <eugney.yakimovitch@gmail.com>
2015-07-07 16:23:30 +0200

    Adding LIB override option to enable running custom code for createjpgs().

--- iBRAIN/bin/createjpgs.sh
+++ iBRAIN/bin/createjpgs.sh
@@ -2,6 +2,9 @@
 echo submitting jpg creation on $1
 REPORTFILE=CreateJPGs_$(date +"%y%m%d%H%M%S").results
 bsub -W 08:00 -o $2/$REPORTFILE "matlab -singleCompThread -nodisplay -nojvm << M_PROG;
+% Add custom project code support.
+brainy.libpath.checkAndAppendLibPath(os.path.dirname(batchfile));
+
 create_jpgs('$1','$2');
 merge_jpgs_per_plate('$2');
 M_PROG"
